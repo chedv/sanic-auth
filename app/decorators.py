6787584a28906a54f555ae2e28ccb41d885ff733
Dmytro Chernov <chernovdv001@gmail.com>
2020-11-09 16:00:09 +0200

    updated response

--- app/decorators.py
+++ app/decorators.py
@@ -1,13 +1,13 @@
 from app.user_session import get_session
-from sanic.exceptions import abort
+from app import responses
 
 
 def login_required(handler):
     def wrapper(self, request):
         user = request.args['user']
         if user is None:
-            abort(status_code=401)
+            return responses.unauthorized
         if get_session(user) is None:
-            abort(status_code=401)
+            return responses.unauthorized
         return handler(self, request)
     return wrapper
